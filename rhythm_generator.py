eeeb2f7cc5adf6bed4db8ddbe34f2c2bf323c223
Christian Clark <christianclark687@gmail.com>
2019-09-24 21:50:19 -0500

    added ability to store default rhythmic and time signature values for preset levels

--- rhythm_generator.py
+++ rhythm_generator.py
@@ -1,11 +1,34 @@
+#basic stuff needed to import from other files
 import random
+
+lp_template = open("resources/lilypond_template.txt", "r")
+template_text = lp_template.read()
+
+ts_file = "resources/time_signatures.txt"
+time_signatures = []
+with open(ts_file) as file:
+    line = file.readline().rstrip()
+    count = 1
+    while line:
+        time_signatures.append(line)
+        print(time_signatures)
+        line = file.readline().rstrip()
+        count += 1
+
+rthm_file = "resources/rhythmic_values.txt"
+rhythmic_values = []
+with open(rthm_file) as file:
+    line = file.readline().rstrip()
+    count = 1
+    while line:
+        rhythmic_values.append(line)
+        print(rhythmic_values)
+        line = file.readline().rstrip()
+        count += 1
 
 #stuff needed to write a new lilypond file as the output
 file_name = "lilypond-test-" + (str(random.randrange(0, 1000, 1)) + ".ly")
 print(file_name)
-
-lp_template = open("resources/lilypond_template.txt", "r")
-template_text = lp_template.read()
 
 def make_lilypond(notes):
     """writes the given notes to a new lilypond file with a random name"""
@@ -17,8 +40,28 @@ def make_lilypond(notes):
     return
 
 #this actually figures out what notes to put
-
 class Level(object):
-    def __init__(time_signatures, note_values, pickup)
+    """used to set parameters for sightreading piece with preset data or to
+    save a custom preset group of parameters set by the user"""
+    def __init__(time_signatures, note_values, pickup):
+        self.signatures = time_signatures
+        self.rhythms = note_values
+        self.pickup = pickup
+        return
+    def get_signatures(self):
+        return self.signatures
+    def get_rhythms(self):
+        return self.rhythms
+    def get_pickup(self):
+        return self.pickup
 
-make_lilypond("test")
+"""level_counter = 1
+while level_counter < 5:
+    lvl_time_sig = 0
+    lvl_note_values = 0
+    lvl_pick_up = 0
+    level_to_add = Level(lvl_time_sig, lvl_note_values, lvl_pick_up)
+    level_counter += 1"""
+
+#make_lilypond("test")
+lp_template.close()
