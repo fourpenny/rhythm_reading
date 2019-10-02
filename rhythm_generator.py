5cce147216ad266a904ea3d5f56f2296081d881b
Christian Clark <christianclark687@gmail.com>
2019-10-01 21:24:23 -0500

    can set the time signature and generate a measure

--- rhythm_generator.py
+++ rhythm_generator.py
@@ -11,7 +11,6 @@ with open(ts_file) as file:
     count = 1
     while line:
         time_signatures.append(line)
-        print(time_signatures)
         line = file.readline().rstrip()
         count += 1
 
@@ -22,7 +21,6 @@ with open(rthm_file) as file:
     count = 1
     while line:
         rhythmic_values.append(line)
-        print(rhythmic_values)
         line = file.readline().rstrip()
         count += 1
 
@@ -36,17 +34,19 @@ def make_lilypond(notes):
     global template_text
     new_file = open(file_name, "w+")
     new_file.write(template_text)
+    new_file.write(make_measure)
     new_file.close()
     return
 
-#this actually figures out what notes to put
+#define the level class and create default levels
 class Level(object):
     """used to set parameters for sightreading piece with preset data or to
     save a custom preset group of parameters set by the user"""
-    def __init__(time_signatures, note_values, pickup):
+    def __init__(self, time_signatures, note_values, pickup):
         self.signatures = time_signatures
         self.rhythms = note_values
         self.pickup = pickup
+        self.name = None
         return
     def get_signatures(self):
         return self.signatures
@@ -54,14 +54,58 @@ class Level(object):
         return self.rhythms
     def get_pickup(self):
         return self.pickup
+    def set_name(self, name):
+        self.name = name
+        return
+    def __str__(self):
+        return "Level " + str(self.name) + " has the time signatures: " + str(self.signatures) + " and will have the rhythms: " + str(self.rhythms)
 
-"""level_counter = 1
-while level_counter < 5:
-    lvl_time_sig = 0
-    lvl_note_values = 0
-    lvl_pick_up = 0
+default_levels = []
+level_counter = 0
+while level_counter <= 4:
+    lvl_time_sig = []
+    for signatures in time_signatures:
+        if time_signatures.index(signatures) <= level_counter:
+            lvl_time_sig.append(signatures.split())
+    lvl_note_values = []
+    for rhythms in rhythmic_values:
+        if rhythmic_values.index(rhythms) <= level_counter:
+            lvl_note_values.append(rhythms.split())
+    if level_counter < 2:
+        lvl_pick_up = False
+    else:
+        lvl_pick_up = True
     level_to_add = Level(lvl_time_sig, lvl_note_values, lvl_pick_up)
-    level_counter += 1"""
+    level_to_add.set_name(level_counter)
+    default_levels.append(level_to_add)
+    #for level in default_levels:
+    #    print(level)
+    level_counter += 1
 
+def set_time_sig(level):
+    pick_group = level.get_signatures()
+    if len(pick_group) > 1:
+        pick_sig = pick_group[random.randrange(len(pick_group)-1)]
+    else:
+        pick_sig = pick_group[0]
+        return pick_sig[random.randrange(len(pick_sig)-1)]
+
+time_sig = set_time_sig(default_levels[0])
+
+#make a measure using given parameters
+def make_measure(ts, paras):
+    split_sig = ts.split("/")
+    beat = split_sig[0]
+    beat_note = split_sig[1]
+    notes_in_meas = []
+    return notes_in_meas
+
+#the generator itself
+
+def generate_score(level):
+    return
+
+#print(make_measure(time_sig, None))
+#generate_score()
 #make_lilypond("test")
 lp_template.close()
